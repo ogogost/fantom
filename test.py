d4e1e12919d306dd882125bd637fec41680e6812
ogogost <galsan7@gmail.com>
2022-06-03 00:46:51 +0300

    сортировка по buy/sell

--- test.py
+++ test.py
@@ -40,43 +40,48 @@ class Ui_MainWindow(object):
 
         # button 2
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(40, 120, 100, 30))
+        self.pushButton_2.setGeometry(QtCore.QRect(40, 120, 200, 30))
         self.pushButton_2.setText("Создание БД")
 
         # button 3
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(40, 160, 100, 30))
+        self.pushButton_3.setGeometry(QtCore.QRect(40, 160, 200, 30))
         self.pushButton_3.setText("Создание таблицы")
 
         # button 4
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(1040, 200, 100, 30))
+        self.pushButton_4.setGeometry(QtCore.QRect(940, 200, 200, 30))
         self.pushButton_4.setText("Запись в таблицу")
 
         # button 5
         self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setGeometry(QtCore.QRect(1040, 240, 100, 30))
+        self.pushButton_5.setGeometry(QtCore.QRect(940, 240, 200, 30))
         self.pushButton_5.setText("Вывод таблицы")
 
         # button 6
         self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_6.setGeometry(QtCore.QRect(40, 280, 100, 30))
+        self.pushButton_6.setGeometry(QtCore.QRect(40, 280, 200, 30))
         self.pushButton_6.setText("Удаление таблицы")
 
         # button 7
         self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setGeometry(QtCore.QRect(40, 320, 100, 30))
+        self.pushButton_7.setGeometry(QtCore.QRect(40, 320, 200, 30))
         self.pushButton_7.setText("Удаление БД")
 
         # button 8
         self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_8.setGeometry(QtCore.QRect(1040, 360, 100, 30))
+        self.pushButton_8.setGeometry(QtCore.QRect(940, 360, 200, 30))
         self.pushButton_8.setText("Добавка строки")
+
+        # button 9
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(340, 460, 200, 30))
+        self.pushButton_9.setText("Сортировка")
 
         # qline1
         self.lineEdit_1 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_1.setGeometry(QtCore.QRect(1300, 200, 200, 40))
-        self.lineEdit_1.setText("Jhon")
+        self.lineEdit_1.setGeometry(QtCore.QRect(1300, 200, 200, 30))
+        self.lineEdit_1.setText("John")
         # self.lineEdit_1.setInputMask('HHH:HHH')
         self.lineEdit_1.setMaxLength(100)
         self.label_of_lineEdit_1 = QtWidgets.QLabel(self.centralwidget)
@@ -195,6 +200,8 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.pushButton_8.clicked.connect(self.add_data_to_table)
         # self.ui.pushButton_8.clicked.connect(self.lineEdit_1_event)
         # self.ui.pushButton_8.clicked.connect(self.add_data_to_table(4, 'M', 'market', 'buy', 800, 5, 'YNDX', 'time'))
+        self.ui.pushButton_9.clicked.connect(self.sort_too)
+
 
         self.ui.radiobutton_ML_1.toggled.connect(self.rb_ml_1)
         self.ui.radiobutton_ML_2.toggled.connect(self.rb_ml_2)
@@ -204,6 +211,40 @@ class mywindow(QtWidgets.QMainWindow):
 
         self.initUi()
 
+    def sort_too(self):
+        con = sl.connect(data_base_path)
+        cursor = con.cursor()
+        try:
+            cursor.execute("SELECT * FROM TABLE_OF_ORDERS")
+            product = cursor.fetchall()  # вытаскиваем содержимое бд в виде списка
+            buy_list = [] # создаём пустые списки
+            sell_list = [] # создаём пустые списки
+            for i in product: # данный цикл добавляет строки из бд по наличию слова BUY
+                if i[3] == 'BUY':
+                    buy_list.append(i)
+                else:
+                    sell_list.append(i)
+
+            # сортируем каждый список по величине цены
+            buy_list_sorted = sorted(buy_list, key=lambda buy_list:buy_list[4], reverse=True)
+            sell_list_sorted = sorted(sell_list, key=lambda sell_list:sell_list[4])
+
+            # нужна сортировка при одинаковой цене, по времени заявки!
+
+            a = '\n'.join(map(str, buy_list_sorted))
+            b = '\n'.join(map(str, sell_list_sorted))
+
+            self.ui.status_monitor.setText(str(a) + '\n' + str(b))
+            # self.ui.status_monitor.setText(str(product))
+            con.commit()
+            con.close()
+
+            self.statusBar().showMessage('table_of_orders is sorted')
+
+        except Exception as e:
+            print(e)
+            self.statusBar().showMessage(str(e))
+        con.close()
     def rb_ml_1(self):
         self.ui.lineEdit_2.setText('Market')
     def rb_ml_2(self):
@@ -253,9 +294,9 @@ class mywindow(QtWidgets.QMainWindow):
         con = sl.connect(data_base_path)
         try:
             sql = 'INSERT INTO TABLE_OF_ORDERS (id, name_client, order_type, buy_sell, price, amount, ticker, datetime) values(null, ?, ?, ?, ?, ?, ?, ?)'
-            data = [('Alice', 'market', 'buy', 900, 10, 'YNDX', datetime.today()),
-                    ('Bob', 'market', 'sell', 1100, 1, 'YNDX', datetime.today()),
-                    ('Mikle', 'market', 'buy', 800, 5, 'YNDX', datetime.today())]
+            data = [('Alice', 'market', 'BUY', 900, 10, 'YNDX', datetime.today()),
+                    ('Bob', 'market', 'SELL', 1100, 1, 'YNDX', datetime.today()),
+                    ('Mikle', 'market', 'BUY', 800, 5, 'YNDX', datetime.today())]
             # sql = 'INSERT INTO TABLE_OF_ORDERS (id, name_client, order_type, buy_sell, price, amount, ticker, datetime) values(?, ?, ?, ?, ?, ?, ?, ?)'
             # data = [(1, 'Alice', 'market', 'buy', 900, 10, 'YNDX', datetime.today()),
             #         (2, 'Bob', 'market', 'sell', 1100, 1, 'YNDX', datetime.today()),
