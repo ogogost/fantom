8d55c3fb0b3578f96fe5af4bc04324f94addd277
Ogogost <Wizard132>
2022-06-16 15:58:05 +0300

    160622 удаление минорного ордера

--- test14.py
+++ test14.py
@@ -1,2 +1,17 @@
-a = 14 * 2
-print(a)
+data = [(1, 'xxx', 90), (2, 'ccc', 60), (3, 'eee', 70)]
+print(data)
+prices = []
+for i in data:
+    prices.append(i[2])
+
+print(prices)
+
+amount = min(prices)
+print(amount)
+
+print(any(e[2] == amount for e in data))
+
+
+for i in data:
+    if i[2] == amount:
+        print(i[0])
